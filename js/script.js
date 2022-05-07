4f513309e24b7763eb63c322bb0174ce54a28f53
Felipe Lunardelli <felipelunardelli@outlook.com>
2022-05-07 15:45:44 -0300

    Commit & Publish

---
+++ js/script.js
@@ -0,0 +1,149 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Definição dos botões.
+    const menu_icon = document.querySelector('#menuativo');
+    const close_icon = document.querySelector('.icon--fechar');
+
+    // Ações a serem tomadas na ativação do menu.
+    function menuopen() {
+        document.querySelector('.menu--fechado').classList.toggle('menufechadoativo');
+    };
+
+    function menuclose() {
+        document.querySelector('.menu--fechado').classList.toggle('menufechadoativo');
+    };
+
+    menu_icon.onclick = () => {
+        menuopen()
+        $('.circle').removeClass("circledark");
+        $('.cursor').removeClass("cursordark");
+    };
+
+   close_icon.onclick = () => {
+        menuclose()
+        $('.circle').addClass("circledark");
+        $('.cursor').addClass("cursordark");
+    };
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var cerchio = document.querySelectorAll('.cerchio');
+  
+    cerchio.forEach(function (elem) {
+      $(document).on('mousemove touch', function (e) {
+        magnetize(elem, e);
+      });
+    })
+  
+    function magnetize(el, e) {
+      var mX = e.pageX,
+        mY = e.pageY;
+      const item = $(el);
+  
+      const customDist = item.data('dist') * 20 || 120;
+      const centerX = item.offset().left + (item.width() / 2);
+      const centerY = item.offset().top + (item.height() / 2);
+  
+      var deltaX = Math.floor((centerX - mX)) * -0.45;
+      var deltaY = Math.floor((centerY - mY)) * -0.45;
+  
+      var distance = calculateDistance(item, mX, mY);
+  
+      if (distance < customDist) {
+        TweenMax.to(item, 0.5, {
+          y: deltaY,
+          x: deltaX,
+          scale: 1.25
+        });
+        item.addClass('magnet');
+      } else {
+        TweenMax.to(item, 0.6, {
+          y: 0,
+          x: 0,
+          scale: 1
+        });
+        item.removeClass('magnet');
+      }
+    }
+  
+    function calculateDistance(elem, mouseX, mouseY) {
+      return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left + (elem.width() / 2)), 2) + Math.pow(mouseY - (elem.offset().top + (elem.height() / 2)), 2)));
+    }
+  
+  
+  
+  
+  
+    function lerp(a, b, n) {
+      return (1 - n) * a + n * b
+    }
+  
+    class Cursor {
+      constructor() {
+        this.bind()
+        this.cursor = document.querySelector('.js-cursor')
+  
+        this.mouseCurrent = {
+          x: 0,
+          y: 0
+        }
+  
+        this.mouseLast = {
+          x: this.mouseCurrent.x,
+          y: this.mouseCurrent.y
+        }
+  
+        this.rAF = undefined
+      }
+  
+      bind() {
+        ['getMousePosition', 'run'].forEach((fn) => this[fn] = this[fn].bind(this))
+      }
+  
+      getMousePosition(e) {
+        this.mouseCurrent = {
+          x: e.clientX,
+          y: e.clientY
+        }
+      }
+  
+      run() {
+        this.mouseLast.x = lerp(this.mouseLast.x, this.mouseCurrent.x, 0.2)
+        this.mouseLast.y = lerp(this.mouseLast.y, this.mouseCurrent.y, 0.2)
+  
+        this.mouseLast.x = Math.floor(this.mouseLast.x * 100) / 100
+        this.mouseLast.y = Math.floor(this.mouseLast.y * 100) / 100
+  
+        this.cursor.style.transform = `translate3d(${this.mouseLast.x}px, ${this.mouseLast.y}px, 0)`
+  
+        this.rAF = requestAnimationFrame(this.run)
+      }
+  
+      requestAnimationFrame() {
+        this.rAF = requestAnimationFrame(this.run)
+      }
+  
+      addEvents() {
+        window.addEventListener('mousemove', this.getMousePosition, false)
+      }
+  
+      on() {
+        this.addEvents()
+  
+        this.requestAnimationFrame()
+      }
+  
+      init() {
+        this.on()
+      }
+    }
+  
+    const cursor = new Cursor()
+  
+    cursor.init();
+  
+    $(".target").hover(function () {
+      $(".circle").addClass("circlehover")
+    }, function () {
+      $(".circle").removeClass("circlehover")
+    });
+  });
